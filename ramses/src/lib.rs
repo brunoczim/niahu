cf05907fb023ace53bec5812dbcd93e927dc3d92
brunoczim <brunoczim@gmail.com>
2019-09-18 18:34:17 -0300

    ramses almost ready... meanwhile, ahmes need edits with save

--- ramses/src/lib.rs
+++ ramses/src/lib.rs
@@ -11,10 +11,10 @@ use std::{
 
 /// Opcode of NOP
 pub const NOP: u8 = 0x0;
-/// Opcode of STA _reg_, _am_
-pub const STA: u8 = 0x10;
-/// Opcode of LDA _reg_, _am_
-pub const LDA: u8 = 0x20;
+/// Opcode of STR _reg_, _am_
+pub const STR: u8 = 0x10;
+/// Opcode of LDR _reg_, _am_
+pub const LDR: u8 = 0x20;
 /// Opcode of ADD _reg_, _am_
 pub const ADD: u8 = 0x30;
 /// Opcode of OR _reg_, _am_
@@ -23,6 +23,8 @@ pub const OR: u8 = 0x40;
 pub const AND: u8 = 0x50;
 /// Opcode of NOT _reg_
 pub const NOT: u8 = 0x60;
+/// Opcode of SUB _reg_, _am_
+pub const SUB: u8 = 0x70;
 /// Opcode of JMP _am_
 pub const JMP: u8 = 0x80;
 /// Opcode of JN _am_
@@ -39,6 +41,15 @@ pub const NEG: u8 = 0xD0;
 pub const SHR: u8 = 0xE0;
 /// Opcode of HLT
 pub const HLT: u8 = 0xF0;
+
+pub const REG_A: u8 = 0x0;
+pub const REG_B: u8 = 0x1;
+pub const REG_X: u8 = 0x2;
+
+pub const MODE_DIRECT: u8 = 0x0;
+pub const MODE_INDIRECT: u8 = 0x1;
+pub const MODE_IMMEDIATE: u8 = 0x2;
+pub const MODE_INDEXED: u8 = 0x3;
 
 const MEM_HEADER: [u8; 4] = [0x03, 0x52, 0x4D, 0x53];
 const STATE_HEADER: [u8; 4] = [0x04, 0x52, 0x4D, 0x53];
@@ -70,11 +81,11 @@ impl InstrInfo {
             NOP => {
                 Some(Self { mnemonic: "NOP", register: false, operand: false })
             },
-            STA => {
-                Some(Self { mnemonic: "STA", register: true, operand: true })
+            STR => {
+                Some(Self { mnemonic: "STR", register: true, operand: true })
             },
-            LDA => {
-                Some(Self { mnemonic: "LDA", register: true, operand: true })
+            LDR => {
+                Some(Self { mnemonic: "LDR", register: true, operand: true })
             },
             ADD => {
                 Some(Self { mnemonic: "ADD", register: true, operand: true })
@@ -113,10 +124,15 @@ impl InstrInfo {
 pub struct Machine {
     ri: u8,
     pc: u8,
+    rr: u8,
+    rm: u8,
     ra: u8,
     rb: u8,
     rx: u8,
     mem: [u8; 256],
+    negative: bool,
+    zero: bool,
+    carry: bool,
     cycling: bool,
     cycles: u64,
     accesses: u64,
@@ -135,6 +151,47 @@ impl Machine {
     pub fn write(&mut self, addr: u8, data: u8) {
         self.accesses = self.accesses.saturating_add(1);
         self.mem[addr as usize] = data;
+    }
+
+    pub fn update_flags(&mut self, byte: u8) {
+        self.zero = byte == 0;
+        self.negative = byte & 0x80 != 0;
+    }
+
+    pub fn read_register(&mut self) -> u8 {
+        let data = match self.rr {
+            REG_A => self.ra,
+            REG_B => self.rb,
+            REG_X => self.rx,
+            _ => return 0,
+        };
+
+        self.update_flags(data);
+
+        data
+    }
+
+    pub fn write_register(&mut self, data: u8) {
+        self.update_flags(data);
+        match self.rr {
+            REG_A => self.ra = data,
+            REG_B => self.rb = data,
+            REG_X => self.rx = data,
+            _ => (),
+        }
+    }
+
+    fn resolve_mode(&mut self) -> u8 {
+        match self.rm {
+            MODE_DIRECT => self.read(self.ri),
+            MODE_INDIRECT => {
+                let addr = self.read(self.ri);
+                self.read(addr)
+            },
+            MODE_IMMEDIATE => self.ri,
+            MODE_INDEXED => self.read(self.ri).wrapping_add(self.rx),
+            _ => panic!("Invalid mode, but no space for the user to give it"),
+        }
     }
 
     pub fn set_pc(&mut self, data: u8) {
@@ -166,17 +223,25 @@ impl Machine {
     }
 
     pub fn decode_exec(&mut self) {
+        self.rm = self.ri & 0x3;
+        self.rr = self.ri >> 2 & 0x3;
+
         match self.ri & 0xF0 {
             NOP => self.exec_nop(),
-            STA => self.exec_sta(),
-            LDA => self.exec_lda(),
+            STR => self.exec_str(),
+            LDR => self.exec_ldr(),
             ADD => self.exec_add(),
             OR => self.exec_or(),
             AND => self.exec_and(),
             NOT => self.exec_not(),
+            SUB => self.exec_sub(),
             JMP => self.exec_jmp(),
             JN => self.exec_jn(),
             JZ => self.exec_jz(),
+            JC => self.exec_jc(),
+            JSR => self.exec_jsr(),
+            NEG => self.exec_neg(),
+            SHR => self.exec_shr(),
             HLT => self.exec_hlt(),
             _ => (),
         }
@@ -184,52 +249,108 @@ impl Machine {
 
     fn exec_nop(&mut self) {}
 
-    fn exec_sta(&mut self) {
+    fn exec_str(&mut self) {
         self.fetch();
-        self.write(self.ri, self.ra);
+        let addr = self.resolve_mode();
+        let data = self.read_register();
+        self.write(addr, data);
     }
 
-    fn exec_lda(&mut self) {
+    fn exec_ldr(&mut self) {
         self.fetch();
-        self.ra = self.read(self.ri);
+        let addr = self.resolve_mode();
+        let data = self.read(addr);
+        self.write_register(data);
     }
 
     fn exec_add(&mut self) {
         self.fetch();
-        self.ra = self.ra.wrapping_add(self.read(self.ri));
+        let addr = self.resolve_mode();
+        let left = self.read_register();
+        let right = self.read(addr);
+        let (result, carry) = left.overflowing_add(right);
+        self.carry = carry;
+        self.write_register(result);
     }
 
     fn exec_or(&mut self) {
         self.fetch();
-        self.ra |= self.read(self.ri);
+        let addr = self.resolve_mode();
+        let left = self.read_register();
+        let right = self.read(addr);
+        self.write_register(left | right);
     }
 
     fn exec_and(&mut self) {
         self.fetch();
-        self.ra &= self.read(self.ri);
+        let addr = self.resolve_mode();
+        let left = self.read_register();
+        let right = self.read(addr);
+        self.write_register(left & right);
     }
 
     fn exec_not(&mut self) {
-        self.ra = !self.ra;
+        let data = self.read_register();
+        self.write_register(!data);
+    }
+
+    fn exec_sub(&mut self) {
+        self.fetch();
+        let addr = self.resolve_mode();
+        let left = self.read_register();
+        let right = self.read(addr);
+        let (result, borrow) = left.overflowing_sub(right);
+        self.carry = !borrow;
+        self.write_register(result);
     }
 
     fn exec_jmp(&mut self) {
         self.fetch();
-        self.pc = self.ri;
+        self.pc = self.resolve_mode();
     }
 
     fn exec_jn(&mut self) {
         self.fetch();
-        if self.ra & 0x80 != 0 {
-            self.pc = self.ri;
+        let addr = self.resolve_mode();
+        if self.negative {
+            self.pc = addr;
         }
     }
 
     fn exec_jz(&mut self) {
         self.fetch();
-        if self.ra == 0 {
-            self.pc = self.ri;
+        let addr = self.resolve_mode();
+        if self.zero {
+            self.pc = addr;
         }
+    }
+
+    fn exec_jc(&mut self) {
+        self.fetch();
+        let addr = self.resolve_mode();
+        if self.carry {
+            self.pc = addr;
+        }
+    }
+
+    fn exec_jsr(&mut self) {
+        self.fetch();
+        let addr = self.resolve_mode();
+        self.write(addr, self.pc);
+        self.pc = addr.wrapping_add(1);
+    }
+
+    fn exec_neg(&mut self) {
+        let data = self.read_register();
+        self.carry = data == 0;
+        self.write_register((!data).wrapping_add(1));
+    }
+
+    fn exec_shr(&mut self) {
+        self.fetch();
+        let data = self.read_register();
+        self.carry = data & 1 != 0;
+        self.write_register(data >> 1);
     }
 
     fn exec_hlt(&mut self) {
@@ -276,6 +397,8 @@ impl Machine {
         output.write_all(&STATE_HEADER)?;
 
         output.write_all(&[self.ri, self.pc, self.ra])?;
+        output.write_all(&[if self.negative { 1 } else { 0 }])?;
+        output.write_all(&[if self.zero { 1 } else { 0 }])?;
         output.write_all(&[if self.cycling { 1 } else { 0 }])?;
         output.write_all(&self.cycles.to_le_bytes())?;
         output.write_all(&self.accesses.to_le_bytes())?;
@@ -302,7 +425,13 @@ impl Machine {
         self.ri = buf[4];
         self.pc = buf[5];
         self.ra = buf[6];
-        self.cycling = buf[7] != 0;
+        self.negative = buf[7] != 0;
+
+        input.read_exact(&mut buf[.. 3])?;
+
+        self.zero = buf[0] != 0;
+        self.carry = buf[1] != 0;
+        self.cycling = buf[2] != 0;
 
         input.read_exact(&mut buf)?;
         self.cycles = u64::from_le_bytes(buf);
@@ -480,10 +609,15 @@ impl Default for Machine {
     fn default() -> Self {
         Self {
             ri: 0,
+            rr: 0,
+            rm: 0,
             pc: 0,
             ra: 0,
             rb: 0,
             rx: 0,
+            negative: false,
+            zero: false,
+            carry: false,
             mem: [0; 256],
             cycling: false,
             cycles: 0,
