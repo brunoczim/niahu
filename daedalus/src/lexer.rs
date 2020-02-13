bb9e000d449a6166a704ad8c45a96d37f49c06f5
brunoczim <brunoczim@gmail.com>
2020-02-13 08:47:57 -0300

    added number lexing

--- daedalus/src/lexer.rs
+++ daedalus/src/lexer.rs
@@ -23,6 +23,49 @@ impl fmt::Display for BadChar {
     }
 }
 
+#[derive(Debug, Clone, Copy, Fail)]
+/// Invalid hex number is in the source.
+pub struct BadHex {
+    /// Place where the error occured.
+    pub location: Location,
+}
+
+impl fmt::Display for BadHex {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}: Hex number must end in \"h\" or \"H\"", self.location)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Fail)]
+/// Invalid decimal number is in the source.
+pub struct BadDecimal {
+    /// Place where the error occured.
+    pub location: Location,
+}
+
+impl fmt::Display for BadDecimal {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            fmt,
+            "{}: Decimal number must not end in \"h\" or \"H\"",
+            self.location
+        )
+    }
+}
+
+#[derive(Debug, Clone, Copy, Fail)]
+/// Invalid number (too big) is in the source.
+pub struct NumberTooBig {
+    /// Place where the error occured.
+    pub location: Location,
+}
+
+impl fmt::Display for NumberTooBig {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}: Number is too big", self.location)
+    }
+}
+
 /// A kind of a token.
 #[derive(Debug, Clone)]
 pub enum TokenKind<'buf> {
@@ -32,6 +75,8 @@ pub enum TokenKind<'buf> {
     Newline,
     /// This token is an idenitifer.
     Ident(&'buf [u8]),
+    /// This token is a number literal.
+    Number(u16),
 }
 
 /// A token, with a given kind and location of occurence.
@@ -78,6 +123,42 @@ fn is_ident_part(ch: u8) -> bool {
     ch.is_ascii_alphanumeric() || ch == b'_'
 }
 
+/// Tests if the byte is a character that starts a number.
+fn is_number_start(ch: u8) -> bool {
+    ch.is_ascii_digit()
+}
+
+/// Tests if the byte is a character that is part of a number.
+fn is_number_part(ch: u8) -> bool {
+    ch.is_ascii_hexdigit()
+}
+
+/// Tests if the byte is a character that is end of a hex number.
+fn is_hex_number_end(ch: u8) -> bool {
+    ch.eq_ignore_ascii_case(&b'h')
+}
+
+/// Tests if the byte is a character that is a hex-exclusive digit.
+fn is_hex_letter_digit(ch: u8) -> bool {
+    ch.is_ascii_hexdigit() && !ch.is_ascii_digit()
+}
+
+/// Converts hex digit to plain number.
+fn read_hex_digit(ch: u8) -> u8 {
+    if ch.is_ascii_digit() {
+        ch - b'0'
+    } else if ch.is_ascii_lowercase() {
+        ch - b'a' + 10
+    } else {
+        ch - b'A' + 10
+    }
+}
+
+/// Converts decimal digit to plain number.
+fn read_dec_digit(ch: u8) -> u8 {
+    ch - b'0'
+}
+
 impl<'buf> Lexer<'buf> {
     /// Handles the case of an error. Advances the cursor after the error.
     fn handle_error<T>(&mut self, position: SrcPosition<'buf>) -> Fallible<T> {
@@ -91,7 +172,9 @@ impl<'buf> Lexer<'buf> {
         position: SrcPosition<'buf>,
     ) -> Fallible<Token<'buf>> {
         self.src_iter.next();
-        while self.src_iter.peek().map_or(false, |pos| is_whitespace(pos.ch)) {
+        while let Some(_) =
+            self.src_iter.peek().filter(|pos| is_whitespace(pos.ch))
+        {
             self.src_iter.next();
         }
         Ok(Token { kind: TokenKind::Whitespace, location: position.location })
@@ -114,7 +197,9 @@ impl<'buf> Lexer<'buf> {
         let mut count = 1;
 
         self.src_iter.next();
-        while self.src_iter.peek().map_or(false, |pos| is_ident_part(pos.ch)) {
+        while let Some(_) =
+            self.src_iter.peek().filter(|pos| is_ident_part(pos.ch))
+        {
             self.src_iter.next();
             count += 1;
         }
@@ -125,10 +210,79 @@ impl<'buf> Lexer<'buf> {
         })
     }
 
+    /// Handles the case of an incoming identifier token.
+    fn handle_number(
+        &mut self,
+        position: SrcPosition<'buf>,
+    ) -> Fallible<Token<'buf>> {
+        let mut count = 1;
+
+        let mut ch = position.ch;
+        let mut hex = false;
+        self.src_iter.next();
+        while let Some(&new_pos) =
+            self.src_iter.peek().filter(|_| is_number_part(ch))
+        {
+            self.src_iter.next();
+            ch = new_pos.ch;
+            if is_hex_letter_digit(ch) {
+                hex = true;
+            }
+            count += 1;
+        }
+
+        let num = if hex {
+            if let None = self
+                .src_iter
+                .peek()
+                .filter(|pos| pos.ch.eq_ignore_ascii_case(&b'h'))
+            {
+                Err(BadHex { location: position.location })?;
+            }
+            self.src_iter.next();
+
+            let mut num = 0u16;
+            for &ch in &position.buffer[.. count] {
+                num = match num
+                    .checked_mul(16)
+                    .and_then(|num| num.checked_add(read_hex_digit(ch) as u16))
+                {
+                    Some(val) => val,
+                    None => Err(NumberTooBig { location: position.location })?,
+                };
+            }
+            num
+        } else {
+            if let None = self
+                .src_iter
+                .peek()
+                .filter(|pos| !pos.ch.eq_ignore_ascii_case(&b'h'))
+            {
+                Err(BadDecimal { location: position.location })?;
+            }
+
+            let mut num = 0u16;
+            for &ch in &position.buffer[.. count] {
+                num = match num
+                    .checked_mul(16)
+                    .and_then(|num| num.checked_add(read_dec_digit(ch) as u16))
+                {
+                    Some(val) => val,
+                    None => Err(NumberTooBig { location: position.location })?,
+                };
+            }
+            num
+        };
+
+        Ok(Token { kind: TokenKind::Number(num), location: position.location })
+    }
+
     /// Skips a comment.
     fn skip_comment(&mut self) -> Fallible<()> {
         self.src_iter.next();
-        while self.src_iter.peek().map_or(false, |pos| !is_newline(pos.ch)) {
+        while let Some(_) =
+            self.src_iter.peek().filter(|pos| !is_newline(pos.ch))
+        {
             self.src_iter.next();
         }
         Ok(())
@@ -154,6 +308,8 @@ impl<'buf> Iterator for Lexer<'buf> {
             self.handle_newline(position)
         } else if is_ident_start(position.ch) {
             self.handle_ident(position)
+        } else if is_number_start(position.ch) {
+            self.handle_number(position)
         } else {
             self.handle_error(position)
         })
